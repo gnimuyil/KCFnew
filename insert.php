1f0ca9a25671c4c555b791acc654cc3816656765
gnimuyil <58717861+gnimuyil@users.noreply.github.com>
2020-04-25 19:09:29 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -25,7 +25,7 @@ $specialChars = preg_match('@[^\w]@', $password);
 if($password != $pswrepeat) echo '<script>alert("Passwords did not match! Try again.")</script>'; 
 else if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) 
 {
-  echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+  echo '<script>alert("Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.")</script>'; 
 }
 else if (!empty($password) || !empty($email) || !empty($pswrepeat)) 
 {
