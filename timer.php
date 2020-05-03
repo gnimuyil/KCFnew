38ef7ca4767df564464ea4f082dea6f644e09913
Bakdaulet1 <52189576+Bakdaulet1@users.noreply.github.com>
2020-05-03 14:56:39 -0400

    Fixed formatting and date display

--- timer.php
+++ timer.php
@@ -5,12 +5,21 @@ include('session.php');
 
 $result=mysqli_query($con, "select * from users where user_id='$session_id'")or die('Error In Session');
 $row=mysqli_fetch_array($result);
-$email=$row['email'];
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="form-wrapper"> 
+    <center><h3> </h3></center>
+	 <?php
+	 $email=$row['email'];
 /* Getting entry date from CentralDatabase */
 $query2= mysqli_query($con, "SELECT * FROM CentralDatabase WHERE email='$email'");
 $row2= mysqli_fetch_array($query2);
 $stamp = $row2['CurrentDate']; 
-echo "You have already submitted an application. Please try again in "; 
+echo "You have already submitted an application for the current application period. You will be able to submit another application on "; 
 /* Preparing timestamps for finding difference */
 $today = date('Y-m-d h:i:s', time());
 $end = date ('Y-m-d h:i:s', strtotime ($stamp ."+365 days")); 
@@ -19,6 +28,10 @@ $ts1 = strtotime($today);
 $ts2 = strtotime($end);      
 $seconds_diff = $ts2 - $ts1;                            
 $time = round(($seconds_diff/(3600*24)));
-echo $time;
-echo " days.";
-?>
+$end = date ('m/d/Y', strtotime ($end));
+echo $end;
+	 ?>
+</div>
+
+</body>
+</html>
