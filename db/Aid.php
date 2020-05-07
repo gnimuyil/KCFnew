1b6c9e67f705110d686614786429e84e0b33f380
Bakdaulet1 <52189576+Bakdaulet1@users.noreply.github.com>
2020-05-06 22:49:43 -0400

    Financial aid form

---
+++ db/Aid.php
@@ -0,0 +1,26 @@
+<?php 
+	require 'database.php';
+	$id = null;
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+	
+	if ( null==$id ) {
+		header("Location: index.php");
+	} else {
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT * FROM CentralDatabase where id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		$data = $q->fetch(PDO::FETCH_ASSOC);
+		Database::disconnect();
+	}
+?>
+<?php
+$result=$data['Aid'];
+header("Content-type: application/pdf");
+$out=base64_decode($result);
+echo $out;
+
+?>
