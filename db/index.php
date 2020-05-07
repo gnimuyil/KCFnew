8d6379ad43b33740ee7bb40151e632f216a9b4ff
Bakdaulet1 <52189576+Bakdaulet1@users.noreply.github.com>
2020-05-07 15:16:43 -0400

    session safety for reviewrs

--- db/index.php
+++ db/index.php
@@ -1,3 +1,12 @@
+<?php 
+include('dbcon.php');
+include('session.php'); 
+
+
+$result=mysqli_query($con, "select * from users where user_id='$session_id'")or die('Error In Session');
+$row=mysqli_fetch_array($result);
+
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
